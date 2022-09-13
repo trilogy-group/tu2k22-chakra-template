d15dc63befd043278648736c870527b951d7eea8
Rishabh <rishabh@Rishabhs-MBP.localdomain>
2022-09-13 10:51:52 +0400

    Updated sample application

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
+import {createRoot} from "react-dom/client"
+import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
 
 const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
-const root = ReactDOM.createRoot(container)
+const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
