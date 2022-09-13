953e24c2d6a50babbee40ce2d356880193634d85
sandesh <sandesh.jain@trilogy.com>
2022-09-13 18:48:49 +0400

    component 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
 import React from 'react'
-import { ChakraProvider, Center, Button, Text } from '@chakra-ui/react'
+import { ChakraProvider, Box, Divider, Text } from '@chakra-ui/react'
 
 const App = () => (
   <ChakraProvider resetCSS>
-    <Center
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="flex-start"
-      m={16}
-      p={8}
-      backgroundColor="cyan.100"
-      bgGradient="linear(to right, green.200,blue.500)"
-    >
-      <Text opacity={1} fontWeight="bold" fontSize="lg" letterSpacing="widest">
-        Some text blah blah blah
-      </Text>
-      <Button
-        variant="ghost"
-        size="md"
-        bgGradient="linear(to right, messenger.500,green.500)"
-        borderRadius={100}
-        border={20}
-      >
-        Test button
-      </Button>
-    </Center>
+    <Box>
+      <Divider borderColor="blackAlpha.500" />
+      <Box backgroundColor="linkedin.500" width={40} mt={10}>
+        <Text color="whiteAlpha.900">Update Profile</Text>
+      </Box>
+    </Box>
   </ChakraProvider>
 )
 
